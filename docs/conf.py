7020166d2983c72d87e8c38897ed26bd3a46d842
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-10-22 12:07:49 -0400

    DOC: Barebones for building sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "EOGLearn"
+copyright = "2023, Scott Huberty"
+author = "Scott Huberty"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx_design",
+    "sphinxemoji.sphinxemoji",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "shibuya"
+html_static_path = ["_static"]
+
+# Enable numref for automatically numbering Figures, i.e "Fig 1"
+numfig = True
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. EOGLearn documentation master file, created by
+   sphinx-quickstart on Sun Oct 22 12:01:52 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to EOGLearn's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
