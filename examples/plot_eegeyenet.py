5b2ef2deb7f38a7618294bc69c44a7cfea76cf42
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-11-06 11:33:31 -0500

    DOC: add model and topo to eegeyenet tut

--- examples/plot_eegeyenet.py
+++ examples/plot_eegeyenet.py
@@ -10,10 +10,31 @@ which presents a subject with a series of dots at fixed locations on the screen.
 
 # %%
 # Import the necessary packages
+# ----------------------------
 from eoglearn.datasets import fetch_eegeyenet
 from eoglearn.io import read_raw_eegeyenet
+from eoglearn.models import EOGDenoiser
+import mne
+
 
 fpath = fetch_eegeyenet()
 fname = fpath / "EP10_DOTS1_EEG.mat"
 raw = read_raw_eegeyenet(fname)
+raw
+
+# %%
+# Plot the raw data
+# -----------------
 raw.plot()
+
+# %%
+# Create and fit model
+# --------------------
+eog_denoiser = EOGDenoiser(raw, downsample=5)
+eog_denoiser.fit(epochs=10)  # limit to 10 epochs for speed
+
+# %%
+# Plot A topomap of the EOG activity
+# ----------------------------------
+montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
+eog_denoiser.plot_eog_topomap(montage=montage)
