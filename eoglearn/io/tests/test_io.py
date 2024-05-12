43522f02e88f042bdc9610ab9b7af50bfc7e9476
Christian O'Reilly <christian.oreilly@gmail.com>
2024-05-12 18:56:14 -0400

    Fix failing tests.

--- eoglearn/io/tests/test_io.py
+++ eoglearn/io/tests/test_io.py
@@ -8,8 +8,7 @@ from eoglearn.io import read_raw_eegeyenet
 def test_read_raw_eegeyenet():
     """Test reading a file from the EEG EyeNet dataset."""
     fpath = fetch_eegeyenet(fetch_dataset_kwargs=dict(force_update=True))
-    fname = fpath / "EP10_DOTS1_EEG.mat"
-    raw = read_raw_eegeyenet(fname)
+    raw = read_raw_eegeyenet(fpath)
     assert raw.info["sfreq"] == 500
     assert len(raw.info["ch_names"]) == 133
     assert raw.get_channel_types().count("eeg") == 129
