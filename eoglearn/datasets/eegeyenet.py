74039c0733bea12623805fdb66f6a248ddfae44e
Christian O'Reilly <christian.oreilly@gmail.com>
2024-05-12 17:33:00 -0400

    Add support to download all EEGEyeNet DOTS data.

--- eoglearn/datasets/eegeyenet.py
+++ eoglearn/datasets/eegeyenet.py
@@ -1,23 +1,40 @@
+import pandas as pd
+from pathlib import Path
+
 from .utils import _fetch_dataset
 
-PARAMS = {
-    "EP10_DOTS": list(
-        [
-            dict(
-                url="https://osf.io/download/u2hct/",
-                archive_name="EP10_DOTS1_EEG.mat",
-                folder_name="EEGEYENET-Data/dots/EP10",
-                hash="md5:3e05afdf9c873ac34cb2ffc1141256a3",
-                dataset_name="EEGEYENET",
-            )
-        ]
-    ),
-}
 
-DOTS = {"EP10": PARAMS["EP10_DOTS"]}
+def _get_urls_df():
+    return pd.read_csv(Path(__file__).parent / "eegeyenet_urls.csv")
 
 
-def fetch_eegeyenet(subject="EP10", run=0, fetch_dataset_kwargs=None):
+def _get_params(subject, run):
+    df = _get_urls_df()
+    row = df.loc[(df.subject == subject.upper()) & (df.run == int(run))]
+    assert len(row) == 1
+    row = row.T.squeeze()
+    return dict(
+                url=row["url"],
+                archive_name=f"{subject}_DOTS{run}_EEG.mat",
+                folder_name=f"EEGEYENET-Data/dots/{subject}",
+                hash=row["hash"],
+                dataset_name="EEGEYENET")
+
+
+def get_subjects_runs():
+    """Get dictionary of {subject: [lists of runs]}.
+
+    Returns
+    -------
+    dict
+        Dictionary of subjects with the runs as values.
+    """
+    df = _get_urls_df()
+    return {subject: df.run.values[df.subject == subject]
+            for subject in df.subject.unique()}
+
+
+def fetch_eegeyenet(subject="EP10", run=1, fetch_dataset_kwargs=None):
     """Fetch a sample file from the EEG Eyenet dataset.
 
     Parameters
@@ -25,11 +42,8 @@ def fetch_eegeyenet(subject="EP10", run=0, fetch_dataset_kwargs=None):
     subject : str
         Subject identifier. Defaults to ``'EP10'``.
     run : int | str
-        Which run to download. Most Participants completed 6 runs of the task, saved
-        to 6 different files. integers are treated as indices, i.e. ``0`` for subject
-        ``"EP10"``corresponds to ``"EP10_DOTS1_EEG.mat"``. If ``str``, it must be the
-        exact filename to download for the ``subject``, ie. ``"EP10_DOTS1_EEG.mat"``.
-        Defaults to ``0``.
+        Which run to download. Most Participants completed 6 runs of the task,
+        saved to 6 different files. Defaults to ``1``.
     fetch_dataset_kwargs : dict | None
         Keyword arguments to pass to :func:`~mne.datasets.fetch_dataset`.
         if ``None``, no keyword arguments are passed. Defaults to ``None``.
@@ -41,13 +55,21 @@ def fetch_eegeyenet(subject="EP10", run=0, fetch_dataset_kwargs=None):
     """
     if not fetch_dataset_kwargs:
         fetch_dataset_kwargs = dict()
-    if isinstance(run, int):
-        dataset_params = DOTS[subject][run]
-    elif isinstance(run, str):
-        run = [run for run in DOTS[subject] if run["archive_name"] == run][0]
-        dataset_params = DOTS[subject][run]
-    else:
-        raise ValueError("run must be an integer or string, not {}".format(type(run)))
-    fetch_dataset_kwargs["dataset_params"] = dataset_params
+    run = int(run)
+    runs = get_subjects_runs()
+    if subject not in runs or run not in runs[subject]:
+        raise ValueError("subject and run not available. See "
+                         "get_subjects_runs() for information on "
+                         "available subjects and runs.")
+
+    fetch_dataset_kwargs["dataset_params"] = _get_params(subject, run)
     fpath = _fetch_dataset(fetch_dataset_kwargs=fetch_dataset_kwargs)
+    fpath = fpath / fetch_dataset_kwargs["dataset_params"]["archive_name"]
+
+    # mne.datasets.fetch_dataset will not download a new run if the subject
+    # folder already exist
+    if not fpath.exists():
+        fetch_dataset_kwargs["force_update"] = True
+        _fetch_dataset(fetch_dataset_kwargs=fetch_dataset_kwargs)
+
     return fpath
